089d78cb35eb4d49917033ed6ec5eae237659deb
AgungRivai002 <141213183+AgungRivai002@users.noreply.github.com>
2023-08-04 10:37:04 +0700

    agung (menghitung panjang karakter)

---
+++ AGUNG RIVAI XI-10 (2).cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <Iostream>
+using namespace std;
+
+int main ()
+{
+//mendeklarisakan array dan variabel
+int A[2] [2], B[2] [3], C[2] [2]
+
+cout  <<  "Masukkan Nilai A11 : ";
+cin>>A[0] [0];
+cout << "Masukan Nilai A12 : ";
+cin>>A[0] [1];
+cout << "Masukan Nilai A21 : ";
+cin>>A[1] [0];
+cout << "Masukan Nilai A23 : ";
+cin>>A[1] [1];
+
+cout << "Masukan Nilai B11 : ";
+cin>>B[0] [0];
+cout << "Masukan Nilai B12 : ";
+cin>>B[0] [0];
+cout << "Masukan Nilai B21 : ";
+cin>>B[1] [0];
+cout << "Masuksn Nilai b23 : ";
+cin>>[1] [1];
+
+//menghitung nilai array
+C[0] [0] = (A[0] [0] * B[0] [0]) + (A[0] [1]) * B[1] [1]);
+C[0] [1] = (A[0] [0] * B[0] [1]) + (A[0][ 1]) * B[1] [1]);
+C[1] [0] = (A[]
+C
